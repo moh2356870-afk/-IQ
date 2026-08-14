703cf8c5839a7c0cdf790f56afcaf9851d0f20e4
moh2356870-afk <moh2356870@gmail.com>
2026-08-14 16:15:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.post("/notifications", (req, res) => {
+  console.log("Salla notification received:", req.body);
+  res.status(200).json({ success: true });
+});
 app.get("/", (req, res) => {
   res.json({
     app: "Matjar IQ",
